aa5f5f884756848b4837042cc507430ac10a2db4
hossamdev <hossamdev14@gmail.com>
2024-03-01 10:10:41 +0100

    create a new note

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -2,30 +2,41 @@
 
 import { FormEvent, useState } from "react";
 import axios from 'axios';
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "@/store";
+import { addNote } from "@/store/notes/noteThunk";
+import { redirect, useRouter } from 'next/navigation'
+import { revalidatePath } from 'next/cache'
+
+
+
 
 const page = () => {
     const [title, setTitle] = useState(""); // I can use client hooks 
     const [content, setContent] = useState("");
-    
+    const [added, setAdded] = useState("");
+    const dispatch = useDispatch<AppDispatch>();
+    const router=useRouter()
+     
+    // const fieldEmpty = () => {
+    //     return title === "" || content === "";
+    // };
+
     const handleSubmit = async (e: FormEvent) => {
-        e.preventDefault();        
-        try{
-            await axios.post('api/notes', { title, content })
-            .then(() => alert('Note Added'));
-            
-            setTitle("");
-            setContent("")
-        } catch(err){
-            console.log(err);
-        }
+        e.preventDefault();
+        dispatch(addNote({title, content}))
+       router.push("/")
+             // Navigate to the new post page
     };
     
     return (
-        <div>
-            <section className="max-w-full bg-slate-950 p-4 mt-4 h-full rounded-md">
-                <h2 className="text-center text-gray-100 text-2xl font-semibold">Create Note</h2>
-                <div className="max-w-lg mx-auto">
-                    <form onSubmit={handleSubmit} className="p-4 bg-gray-900 mt-5 mb-4 rounded-md flex flex-col">
+        <div className="w-full flex justify-center">
+            <section className="w-6/12 min-w-96 max-w-xl bg-slate-950 p-4 mt-4 h-full rounded-md ">
+                <h2 className="text-center text-gray-100 text-2xl font-semibold">
+                Create A New Note
+                </h2>
+                <div className="max-w-lg  mx-auto">
+                    <form onSubmit={handleSubmit} className="p-4 text-lg text-gray-800 bg-gray-900 mt-5 mb-4 rounded-md flex flex-col">
                         <div className="mb-4">
                             <input
                                 onChange={(event) => setTitle(event.target.value)}
@@ -43,8 +54,11 @@ const page = () => {
                                 placeholder="Content"
                             ></textarea>
                         </div>
-                        <button type="submit" className="py-2 px-4 bg-green-500 hover:bg-green-400 text-gray-100 rounded-md shadow-sm font-medium">
-                        Add note
+                        <div className="w-full text-center">
+                            <p className="text-green-400 p-4 text-lg">{added}</p>
+                        </div>
+                        <button type="submit" className="py-2 px-4 bg-green-500 hover:bg-green-400 text-gray-300 rounded-md shadow-sm font-medium">
+                            Add note
                         </button>
                     </form>
                 </div>
